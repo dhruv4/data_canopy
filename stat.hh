1c644d2d9df5d8e3fe9369c4328ac60ab4d62f2e
Abdul Wasay <awasay@g.harvard.edu>
2015-06-25 19:28:48 -0400

    stat module addes

---
+++ stat.hh
@@ -0,0 +1,33 @@
+#ifndef STAT_HH
+#define STAT_HH
+
+
+#include <math.h>
+#include <assert.h>
+
+#include "data.hh"
+#include "config.h"
+
+/*
+* 	The struct that contains the stats
+*/
+struct stat{
+
+	pos_int num=0;
+	float mean=0.0;
+	float variance=0.0;
+	float standard_deviation=0.0;
+	float corelation=0.0;
+
+};
+
+
+float calculateCorelation(column* x, column* y);
+float calculateCorelation(data* x, pos_int size_x, data* y, pos_int size_y);
+float calculateMean(data* x, pos_int size_x);
+float calculateVariance(data* x, pos_int size_x,float mean);
+
+
+
+
+#endif
